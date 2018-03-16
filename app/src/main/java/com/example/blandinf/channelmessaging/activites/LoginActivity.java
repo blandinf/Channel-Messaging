0945e65ae5e0df306fb82fd521bc63b61e632096
blandinf <florianblandin-iut@outlook.fr>
2018-03-16 16:48:29 +0100

    Design ended

--- app/src/main/java/com/example/blandinf/channelmessaging/activites/LoginActivity.java
+++ app/src/main/java/com/example/blandinf/channelmessaging/activites/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.blandinf.channelmessaging.activites;
 
+import android.animation.Animator;
 import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,12 +14,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.Handler;
 
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.blandinf.channelmessaging.fragments.ChannelListFragment;
 import com.example.blandinf.channelmessaging.response.AuthenticationResponse;
 import com.example.blandinf.channelmessaging.ws.HttpPostHandler;
@@ -25,20 +33,28 @@ import com.example.blandinf.channelmessaging.ws.OnDownloadListener;
 import com.example.blandinf.channelmessaging.ws.PostRequest;
 import com.example.blandinf.channelmessaging.R;
 import com.google.gson.Gson;
+import com.wang.avi.AVLoadingIndicatorView;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText mail;
     private EditText password;
+    private TextView _textView;
     private ImageView mIvLogo;
     private Button validateBtn;
     private Gson gson = new Gson();
     public static final String PREFS_NAME = "MyPrefsFile";
+    private Handler mHandlerTada;
+    private AVLoadingIndicatorView _avi;
+    private static final String[] explainStringArray = {
+            "Connecte toi pour chatter avec tes amis",
+            "rgegergergerge",
+            "ergegeverv",
+};
 
-    public LoginActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +67,13 @@ public class LoginActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         validateBtn = (Button) findViewById(R.id.validate);
         mIvLogo = (ImageView) findViewById(R.id.mIvLogo);
+        _avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
+        _textView = (TextView) findViewById(R.id.textView);
+
+        AnimationDrawable anim = (AnimationDrawable) findViewById(R.id.myLayout).getBackground();
+
+        anim.setEnterFadeDuration(6000);
+        anim.setExitFadeDuration(2000);
 
         validateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,8 +95,27 @@ public class LoginActivity extends AppCompatActivity {
                         if (login.getResponse().equals("Ok")) {
                             Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(loginIntent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, mIvLogo, "logo").toBundle());
-                        } else
-                            Toast.makeText(getBaseContext(), "Error", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Animation animFadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+                            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.myLayout), "Error", Snackbar.LENGTH_SHORT);
+                            mySnackbar.show();
+                            validateBtn.startAnimation(animFadeOut);
+                            _avi.show();
+                            _textView.setText(explainStringArray[new Random().nextInt(explainStringArray.length)]);
+                            animFadeOut.setAnimationListener(new Animation.AnimationListener(){
+                                @Override
+                                public void onAnimationStart(Animation arg0) {
+                                }
+                                @Override
+                                public void onAnimationRepeat(Animation arg0) {
+                                }
+                                @Override
+                                public void onAnimationEnd(Animation arg0) {
+                                    _avi.hide();
+                                }
+                            });
+                        }
+
                     }
 
                     @Override
@@ -84,5 +126,35 @@ public class LoginActivity extends AppCompatActivity {
                 httpPostHandler.execute(postRequest);
             }
         });
+
+
+        mHandlerTada = new Handler(); // android.os.handler
+        final long mShortDelay = 4000; //milliseconds
+
+        mHandlerTada.postDelayed(new Runnable(){
+            public void run(){
+                YoYo.with(Techniques.Tada)
+                        .duration(700)
+                        .repeat(1)
+                        .playOn(findViewById(R.id.mIvLogo));
+                mHandlerTada.postDelayed(this,mShortDelay);
+            }
+        }, mShortDelay);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AnimationDrawable anim = (AnimationDrawable) findViewById(R.id.myLayout).getBackground();
+        if (anim != null && !anim.isRunning())
+            anim.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AnimationDrawable anim = (AnimationDrawable) findViewById(R.id.myLayout).getBackground();
+        if (anim != null && anim.isRunning())
+            anim.stop();
     }
 }
